3eeb54d4e365ca65d169e4d268708928b55fb013
Matheus Gois <mrochagois@gmail.com>
2021-01-18 20:54:40 -0300

    H2 security configuration

---
+++ Backend/src/main/java/com/vobidu/espacolr/config/ResourceServerConfig.java
@@ -0,0 +1,56 @@
+package com.vobidu.espacolr.config;
+
+import java.util.Arrays;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private JwtTokenStore tokenStore;
+	
+	//Vetores para mostrar as rotar permitidas para cada tipo de usuário
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+	
+	private static final String[] OPERATOR_OR_ADMIN = { "/clients/**", "/scheduleds/**" };
+	
+	private static final String[] ADMIN = { "/users/**" };
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore); //esse metodo vai possibilitar a verificação do token (se ele é valido, se espirou etc)
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		
+		//H2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+		//Criando as rotas permitidas
+		http.authorizeRequests()
+		.antMatchers(PUBLIC).permitAll() //Permitindo todas as rotas do vetor publico
+		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll() //Permitindo todas as rotas do vetor OPERATO_OR_ADMIN no metodo GET
+		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN") //Permitindo as rotas do vetor OPERATOR_OR_ADMIN onde o usuário logado seja do tipo OPERATO OU ADMIN
+		.antMatchers(ADMIN).hasRole("ADMIN") //Permitindo a rota do vetor ADMIN apenas quem tá logao como Admin
+		.anyRequest().authenticated(); //Bloqueando para os usuário sem autenticação qualquer outra rota 
+	}
+
+	
+}
